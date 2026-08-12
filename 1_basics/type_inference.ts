fc3c933bb4c0b679a3dd7870dc0842952e2c6980
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-12 10:21:00 -0400

    legitimate busgs and basic logic

--- 1_basics/type_inference.ts
+++ 1_basics/type_inference.ts
@@ -17,4 +17,25 @@ const user = {
 
 // Property 'age' does not exist on type '{ name: string; gender: string; }'.
 // age... mom, no pun intended
-console.log(user.age);
+// console.log(user.age);
+
+// legitimate bugs
+const announcement = "Hello World!"
+
+// How quickly can you spot the typos?
+// announcement.toLocaleLowercase();
+// announcement.toLocalLowerCase();
+
+console.log(announcement.toLocaleLowerCase());
+
+// logic errors
+
+const value = Math.random() > 0.5 ? "a" : "b"
+
+if (value !== "a") {
+  console.log("b")
+} else if (value === "b") { // This comparison appears to be unintentional because the types '"a"' and '"b"' have no overlap.
+  // unreachable
+  console.log('whatever');
+}
+
