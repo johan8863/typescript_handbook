21547b1ed9bd701b6c9c3ec9733da65da13b06bd
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-12 09:30:25 -0400

    error because of worng calling

---
+++ 1_basics/hello.js
@@ -0,0 +1,6 @@
+const message = "Hello World!";
+// the base problem, on javascript there's no
+// error until runtime because of the lack of static typing
+
+// TypeError: message is not a function
+console.log(message());
