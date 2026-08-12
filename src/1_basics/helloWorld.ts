c383c0baf644f740ac2c350b2835f32b6fc44bc2
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-12 14:23:44 -0400

    allow typescript assign type when possible

--- src/1_basics/helloWorld.ts
+++ src/1_basics/helloWorld.ts
@@ -2,3 +2,8 @@ const greet = (person: string, date: Date): string =>
   `Hello ${person}, today is ${date.toDateString()}!`;
 
 console.log(greet("Gisela", new Date()));
+
+// on cases where the typescript checker can infer
+// the type is better to leave it without typing
+// for example at
+let firstName = "Gisela"; //let firstName: string
