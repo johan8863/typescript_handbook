94e53ee59b8642ad4bed239d84beb05b3f19c416
Johan Travieso Castro <jtravieso8863@gmail.com>
2026-08-24 11:24:39 -0400

    protected visibility

--- src/6_classes/memberVisibility.ts
+++ src/6_classes/memberVisibility.ts
@@ -10,3 +10,26 @@ class Greet {
 
 const g = new Greet();
 g.greet();
+
+// protected
+// protected members are only visible to subclasses of the class they’re declared in.
+// IMPORTANT!, only within the class no through instances
+class Greeter {
+  public greet(): void {
+    console.log(`Hello ${this.getName()}`);
+  }
+
+  protected getName(): string {
+    return "hi!";
+  }
+}
+
+class SpecialGreeter extends Greeter {
+  public howdy(): void {
+    console.log(`Howdy ${this.getName()}`);
+  }
+}
+
+const sg = new SpecialGreeter();
+sg.howdy();
+sg.getName(); // error
